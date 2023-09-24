3febb9f861ff906a24a5e1c6fe63befd59776a48
taliakouncarr <132114273+taliakouncarr@users.noreply.github.com>
2023-09-24 11:13:32 -0400

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,14 @@
+
+public class ArraySum {
+
+	public int sumOfArray (Integer[] a, int index) {
+		
+		if (index < 0) {
+			return 0;
+		}
+		
+		else
+			return a[index] + sumOfArray (a, index -1);
+	}
+	
+}
